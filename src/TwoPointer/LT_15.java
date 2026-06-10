0e93103bc789eb58e77308acd6c2ae41b508a5c2
lalit-0410 <chaudharylalit4444@gmail.com>
2026-06-10 16:20:54 +0530

    update problem

--- src/TwoPointer/LT_15.java
+++ src/TwoPointer/LT_15.java
@@ -5,49 +5,66 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LT_15 {
-    static void uniqueTriplet(int[] nums){
-        Arrays.sort(nums);
-        int n=nums.length;
-        List<int[]> triplet=new ArrayList<>();
 
-        for (int i=0;i<n-2;i++){
-            // Skip duplicate first element
-            if(i>0 && nums[i]==nums[i-1]){
+    static List<List<Integer>> threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> triplet = new ArrayList<>();
+        //i<n-2 because triplet
+        for (int i = 0; i < n - 2; i++) {
+
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            int left=i+1;
-            int right=n-1;
+            int left = i + 1;
+            int right = n - 1;
+            int target = -nums[i];
 
-                while (left<right){
-                int sum=nums[i]+nums[left]+nums[right];
-                if(sum==0){
-                    triplet.add(new int[]{nums[i],nums[left],nums[right]});
+            while (left < right) {
+
+                int sum = nums[left] + nums[right];
+
+                if (sum == target) {
+
+                    triplet.add(Arrays.asList(
+                            nums[i],
+                            nums[left],
+                            nums[right]
+                    ));
+
                     left++;
                     right--;
-                    //skip duplicate
-                    while (left<n && nums[left]==nums[left-1]){
+
+                    // Skip duplicate left values
+                    while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
 
-                    while (right>0 && nums[right]==nums[right+1]){
+                    // Skip duplicate right values
+                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
-                } else if (sum<0) {
+
+                } else if (sum < target) {
                     left++;
-                }
-                else {
+                } else {
                     right--;
                 }
-                }
             }
-        for (int[] p : triplet) {
-            System.out.println(p[0] + " " + p[1]+" "+p[2]);
         }
 
+        return triplet;
     }
+
     public static void main(String[] args) {
-        int[] arr={-1,0,1,2,-1,-4};
-        uniqueTriplet(arr);
+
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+
+        List<List<Integer>> ans = threeSum(arr);
+
+        System.out.println(ans);
     }
 }
--- src/TwoPointer/LT_977.java
+++ src/TwoPointer/LT_977.java
@@ -2,39 +2,59 @@ package TwoPointer;
 
 public class LT_977 {
     static void sortedSquare(int[] nums){
-        int count=0,k=0;
         int n=nums.length;
+        int left=0;
+        int right=n-1;
+        int idx=n-1;
         int[] res=new int[n];
-
-        while (nums[count]<0){
-            count++;
-
-        }
-        int i=count-1,j=count;
-        while (i>=0 && j<n){
-            if(nums[i]*nums[i]<nums[j]*nums[j]){
-            res[k]=nums[i]*nums[i];
-            i--;
-        }
-            else {
-                res[k]=nums[j]*nums[j];
-                j++;
+        while (left<right){
+            if(nums[left]*nums[left]>=nums[right]*nums[right]){
+                res[idx]=nums[left]*nums[left];
+                left++;
             }
-            k++;
+            else {
+                res[idx]=nums[right]*nums[right];
+                right--;
+            }
+            idx--;
         }
-        while (i>=0){
-            res[k++]=nums[i]*nums[i];
-            i--;
+        for(int value:res){
+            System.out.print(value+" ");
+        }
 
-        }
-        while (j<n){
-            res[k++]=nums[j]*nums[j];
-            j++;
-        }
-        for(int a:res){
-            System.out.print(a+" ");
-
-        }
+//        int count=0,k=0;
+//        int n=nums.length;
+//        int[] res=new int[n];
+//
+//        while (nums[count]<0){
+//            count++;
+//
+//        }
+//        int i=count-1,j=count;
+//        while (i>=0 && j<n){
+//            if(nums[i]*nums[i]<nums[j]*nums[j]){
+//            res[k]=nums[i]*nums[i];
+//            i--;
+//        }
+//            else {
+//                res[k]=nums[j]*nums[j];
+//                j++;
+//            }
+//            k++;
+//        }
+//        while (i>=0){
+//            res[k++]=nums[i]*nums[i];
+//            i--;
+//
+//        }
+//        while (j<n){
+//            res[k++]=nums[j]*nums[j];
+//            j++;
+//        }
+//        for(int a:res){
+//            System.out.print(a+" ");
+//
+//        }
     }
     public static void main(String[] args) {
         int[] nums={-4,-1,0,2,4};
