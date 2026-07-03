c9f3a2886d31da1ccf70070ce093713ef5ea51fc
lalit-0410 <chaudharylalit4444@gmail.com>
2026-07-03 21:03:49 +0530

    update 1d

--- src/DP_1/ClimbStair.java
+++ src/DP_1/ClimbStair.java
@@ -1,4 +1,26 @@
 package DP_1;
 
-public class ClimbStair {
+public class ClimbStair
+{
+    public static void main(String[] args)
+    {
+       int n=6;
+        System.out.println(ways(n));
+    }
+
+    private static int ways(int n)
+    {
+        if(n<=2){
+            return n;
+        }
+        int prev=1;
+        int next=2;
+        for (int i = 3; i <=n ; i++) {
+            int temp=prev+next;
+            prev=next;
+            next=temp;
+
+        }
+        return next;
+    }
 }
--- src/DP_1/MinCostClimbStairs.java
+++ src/DP_1/MinCostClimbStairs.java
@@ -1,4 +1,28 @@
 package DP_1;
 
-public class MinCostClimbStairs {
+public class MinCostClimbStairs
+{
+    private static int minCost(int[] cost) {
+        int n=cost.length;
+        return Math.min(solve(n-1,cost),
+                                solve(n-2,cost));
+    }
+    private static int solve(int n,int[] cost){
+        if(n==0){
+            return cost[0];
+        }
+        if(n==1){
+            return cost[1];
+        }
+        return cost[n]+
+                Math.min(solve(n-1,cost)
+                        ,solve(n-2,cost));
+    }
+    public static void main(String[] args)
+    {
+        int [] cost={10,15,20};
+        System.out.println(minCost(cost));
+    }
+
+
 }
